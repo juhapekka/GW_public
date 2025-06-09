e4b037e12b0dc1ec920966c62e906195429dec97
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2025-06-09 23:10:02 +0300

    Gravity Wars game blob!

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+/*
+const CACHE_NAME = "gravitywars-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/client.js",
+  "/alus.png",
+  "/kenttä1.png",
+  "/kenttä2.png",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+*/
+
+// remove the service worker
+self.addEventListener("install", function(event) {
+    self.registration.unregister().then(() => {
+        console.log("✅ Service Worker Unregistered!");
+    });
+});
